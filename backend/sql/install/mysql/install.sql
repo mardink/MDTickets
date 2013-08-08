195a1d52cec94dcbaae7b11fca15c5399c80a5d3
Mardink Webdesign <support@mardinkwebdesign.com>
2013-08-08 19:59:27 +0200

    add sql files

---
+++ backend/sql/install/mysql/install.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE IF NOT EXISTS `#__mdtickets_items` (
+  `mdtickets_item_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `prio` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL DEFAULT '',
+  `short` varchar(255) NOT NULL DEFAULT '',
+  `detail` mediumtext NOT NULL,
+  `requester` varchar(255) NOT NULL DEFAULT '',
+  `deadline` date,
+  `assigned` varchar(255) DEFAULT '',
+  `start_date` date,
+  `itoncall` varchar(55) DEFAULT NULL,
+  `started` tinyint(3) NOT NULL DEFAULT 0,
+  `published` tinyint(3) NOT NULL DEFAULT 1,
+  `status` varchar(255) DEFAULT '',
+  `token` char(32) DEFAULT NULL,
+  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` bigint(20) NOT NULL DEFAULT '0',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` bigint(20) NOT NULL DEFAULT '0',
+  `locked_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `locked_by` bigint(20) NOT NULL DEFAULT '0',
+  `completion_date` date NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `completed_by` varchar(255),
+  `remark` mediumtext NOT NULL,
+  PRIMARY KEY (`mdtickets_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
